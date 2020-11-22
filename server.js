912b1a725f313dd64ff951273b6871f3cc332680
Marcus Hansen <marcusehansen@gmail.com>
2020-11-21 19:47:41 -0600

    connected to mongodb

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 // require our modules and set up our setting variables
 const express = require('express');
-const port = 3005;
+const port = 3009;
 const methodOverride = require('method-override');
+const session = require('express-session');
 const morgan = require('morgan');
 
+// We'll need to load the env vars
+require('dotenv').config();
 
+// require our routes
 const indexRouter = require('./routes/index');
 const picksRouter = require('./routes/picks');
 
+
 // create the express app
 const app = express();
+
+// connect to the MongoDB with mongoose
+require('./config/database');
 
 // configure server settings app.set()
 app.set('view engine', 'ejs');
@@ -17,8 +25,9 @@ app.set('view engine', 'ejs');
 // mount our middleware app.use()
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
+app.use(express.json());
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // own custom middleware
 app.use(function(req, res, next) {
